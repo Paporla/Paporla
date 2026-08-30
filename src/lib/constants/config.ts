802e63b72b887008ba686e8d871e25d991d3528e
Paporla <paporlafoods@gmail.com>
2026-08-30 20:10:25 +0200

    chore: borrar 7 archivos muertos y sus 3 tests huerfanos (s15)

--- src/lib/constants/config.ts
+++
@@ -1,8 +0,0 @@
-export const APP_CONFIG = {
-  name: 'Paporla',
-  description: 'Rescate alimentario',
-  supportPhone: '+56 2 555 1234',
-  supportEmail: 'soporte@paporla.com',
-  pickupCodeLength: 6,
-  reservationCancelationHours: 2,
-}
